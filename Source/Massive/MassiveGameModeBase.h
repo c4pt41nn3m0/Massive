faf2de2aef915041354b6caaeddc20af8e8e9507
Karanveer Singh Thakur <thakur.singh.karanveer@gmail.com>
2022-12-01 03:06:49 +0530

    Initial commit

---
+++ Source/Massive/MassiveGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MassiveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASSIVE_API AMassiveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
